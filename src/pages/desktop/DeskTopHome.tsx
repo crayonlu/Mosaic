d00b8933d622f992efea8aaa11142b77e37fef5b
crayon <1104507145@qq.com>
2025-12-22 00:10:30 +0800

    feat: top bar

--- src/pages/desktop/DeskTopHome.tsx
+++ src/pages/desktop/DeskTopHome.tsx
@@ -2,8 +2,8 @@ import DeskTopLayout from '@/components/layout/DeskTopLayout'
 export default function DeskTopHome() {
   return (
     <DeskTopLayout>
-      <div className="flex">
-        <h1>Home</h1>
+      <div className="flex min-h-screen flex-col">
+        <div className="w-full h-32"></div>
       </div>
     </DeskTopLayout>
   )
