0f0f7e511dd9f6b0893133b3f9f73f9c864b17bd
crayon <1104507145@qq.com>
2025-12-23 14:29:26 +0800

    feat: custom window control

--- src/components/layout/DeskTopLayout.tsx
+++ src/components/layout/DeskTopLayout.tsx
@@ -1,34 +1,16 @@
-import { SidebarProvider, SidebarTrigger, SidebarInset } from '@/components/ui/sidebar'
+import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/desktop/AppSidebar'
 import { useSidebar } from '@/hooks/use-sidebar'
-import { useTime } from '@/hooks/use-time'
-import { userCommands } from '@/utils/callRust'
-import { useEffect, useState } from 'react'
+import { CustomTitleBar } from './CustomTitleBar'
 
 export default function Layout({ children, className }: { children: React.ReactNode, className?: string }) {
   const { sidebarOpen, setSidebarOpen } = useSidebar()
-  const { greeting, formattedDateWithWeek } = useTime()
-  const [username, setUsername] = useState('')
-
-  useEffect(() => {
-    userCommands.getUser().then(user => setUsername(user?.username || '')).catch(console.error)
-  }, [])
+  
   return (
     <SidebarProvider open={sidebarOpen} onOpenChange={setSidebarOpen} defaultOpen={sidebarOpen}>
       <AppSidebar />
       <SidebarInset className='h-screen overflow-hidden'>
-        <header className="flex h-12 shrink-0 items-center justify-between border-b px-4">
-          <div className='flex items-center gap-2'>
-            <SidebarTrigger className="-ml-1 hover:text-primary hover:bg-primary/10 p-2 rounded-md transition-all" />
-            <div className="text-sm text-gray-500">
-              {greeting}
-              {username ? '，' + username : ''}
-            </div>
-          </div>
-          <div className='text-sm text-gray-500'>
-            {formattedDateWithWeek}
-          </div>
-        </header>
+        <CustomTitleBar />
         <div className={`flex-1 p-4 h-[calc(100dvh-3rem)] overflow-y-auto ${className}`}>{children}</div>
       </SidebarInset>
     </SidebarProvider>
