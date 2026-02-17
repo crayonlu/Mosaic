41dbca8e73831eb1a8970b2f4518105d34ed069a
crayon <1104507145@qq.com>
2026-02-17 19:57:19 +0800

    refactor: new

--- mobile/types/index.ts
+++ mobile/types/index.ts
@@ -10,7 +10,7 @@ export { ResourceType } from './resource'
 export type { Resource, ResourcePreview } from './resource'
 
 // Diary Types
-export { MoodKey } from '@/lib/utils/mood'
+export type { MoodKey } from '@mosaic/api'
 export type { Diary, DiaryWithMemos } from './diary'
 
 // User Types
@@ -18,33 +18,34 @@ export type { User } from './user'
 
 // API Types
 export type {
-  ApiError,
-  AuthTokens,
-  CreateDiaryRequest,
-  CreateMemoRequest,
-  DiaryResponse,
-  HealthResponse,
-  HeatMapData,
-  ListDiariesQuery,
-  ListMemosQuery,
-  ListResourcesQuery,
-  LoginRequest,
-  LoginResponse,
-  MemoWithResourcesResponse,
-  MoodData,
-  PaginatedResponse,
-  RefreshTokenRequest,
-  RefreshTokenResponse,
-  SearchMemosQuery,
-  ServerConfig,
-  StatsQuery,
-  SummaryData,
-  SummaryQuery,
-  TimelineData,
-  TrendsData,
-  UpdateDiaryRequest,
-  UpdateMemoRequest
+    ApiError,
+    AuthTokens,
+    CreateDiaryRequest,
+    CreateMemoRequest,
+    DiaryResponse,
+    HealthResponse,
+    HeatMapData,
+    ListDiariesQuery,
+    ListMemosQuery,
+    ListResourcesQuery,
+    LoginRequest,
+    LoginResponse,
+    MemoWithResourcesResponse,
+    MoodData,
+    PaginatedResponse,
+    RefreshTokenRequest,
+    RefreshTokenResponse,
+    SearchMemosQuery,
+    ServerConfig,
+    StatsQuery,
+    SummaryData,
+    SummaryQuery,
+    TimelineData,
+    TrendsData,
+    UpdateDiaryRequest,
+    UpdateMemoRequest
 } from './api'
 
 // Common Types
 export type { PaginatedResponse as BasePaginatedResponse } from './common'
+
