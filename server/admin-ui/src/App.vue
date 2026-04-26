d0674de6e9569696d3ca3f9096279c54faaef88e
crayon <1104507145@qq.com>
2026-04-26 23:15:54 +0800

    refactor: fe

--- server/admin-ui/src/App.vue
+++ server/admin-ui/src/App.vue
@@ -1,33 +1,13 @@
 <template>
-  <n-config-provider :theme="theme">
-    <n-notification-provider>
-      <n-message-provider>
-        <router-view />
-      </n-message-provider>
-    </n-notification-provider>
-  </n-config-provider>
+  <router-view />
+  <ToastProvider />
 </template>
 
 <script setup lang="ts">
-import { darkTheme } from 'naive-ui';
-import { onMounted, onUnmounted, ref } from 'vue';
+import { onMounted } from 'vue';
+import ToastProvider from './components/ToastProvider.vue';
 import { useAuthStore } from './stores/auth';
 
 const auth = useAuthStore();
-
-const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-const theme = ref(mediaQuery.matches ? darkTheme : null);
-
-function onSchemeChange(e: MediaQueryListEvent) {
-  theme.value = e.matches ? darkTheme : null;
-}
-
-onMounted(() => {
-  mediaQuery.addEventListener('change', onSchemeChange);
-  auth.init();
-});
-
-onUnmounted(() => {
-  mediaQuery.removeEventListener('change', onSchemeChange);
-});
+onMounted(() => auth.init());
 </script>
