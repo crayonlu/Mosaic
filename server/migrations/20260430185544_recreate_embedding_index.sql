4c2dc4c730bea6312b86df0825c3195ee8a4ee88
crayon <1104507145@qq.com>
2026-05-01 03:52:02 +0800

    fix: sql

--- server/migrations/20260430185544_recreate_embedding_index.sql
+++ server/migrations/20260430185544_recreate_embedding_index.sql
@@ -1,2 +1,2 @@
 CREATE INDEX IF NOT EXISTS idx_memo_embeddings_embedding
-ON memo_embeddings USING ivfflat (embedding vector_cosine_ops) WITH (lists = 100);
+ON memo_embeddings USING hnsw (embedding vector_cosine_ops);
